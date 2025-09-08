891170e82794a54f1a849fdd37852ec67f12c2c4
Robert Tanjung <robert.tanjung@spesolution.com>
2025-09-08 10:24:41 +0700

    feat: Add new People on Contributors

--- src/configurations/contributors.ts
+++ src/configurations/contributors.ts
@@ -1,7 +1,35 @@
 const contributors = [
   {
+    name: 'Agmar Putra',
+    url: 'https://gitlab.spesolution.net/agmar.putra'
+  },
+  {
+    name: 'Agung Laksono Hartadi',
+    url: 'https://gitlab.spesolution.net/agung.hartadi'
+  },
+  {
+    name: 'Bagus Nur Solayman',
+    url: 'https://gitlab.spesolution.net/bagus.solayman'
+  },
+  {
     name: 'Erghi Imannur Ichsan',
     url: 'https://gitlab.spesolution.net/erghi.ichsan'
+  },
+  {
+    name: 'I Nyoman Arijaya Putra',
+    url: 'https://gitlab.spesolution.net/i.putra'
+  },
+  {
+    name: 'Muhammad Zulfa Dhiaulhaq',
+    url: 'https://gitlab.spesolution.net/muhammad.dhiaulhaq'
+  },
+  {
+    name: 'Nanda Yusuf Nur Pratama',
+    url: 'https://gitlab.spesolution.net/nanda.pratama'
+  },
+  {
+    name: 'Robert Tanjung',
+    url: 'https://gitlab.spesolution.net/robert.tanjung'
   }
 ] as const;
 
