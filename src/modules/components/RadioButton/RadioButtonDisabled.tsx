d7bc4247f34ffcd722d7eb07dfc86fc04cfe4371
agung.hartadi <agung.hartadi@spesolution.com>
2025-09-10 15:11:12 +0700

    feat: Create radio button component

---
+++ src/modules/components/RadioButton/RadioButtonDisabled.tsx
@@ -0,0 +1,23 @@
+import { type ChangeEvent, useState } from 'react';
+import RadioButton from '@/@dront/components/RadioButton';
+
+const RadioButtonDisabled = () => {
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+  };
+
+  return (
+    <RadioButton
+      value={value}
+      handleChange={handleChange}
+      options={[
+        { label: 'Disabled Activated', value: '1', disabled: true },
+        { label: 'Disabled', value: '2', disabled: true }
+      ]}
+    />
+  );
+};
+
+export default RadioButtonDisabled;
