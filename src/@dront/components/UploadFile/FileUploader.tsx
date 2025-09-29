5a3da9f943d83c189c388f8343104c36acd514f6
Bagus Nur Solayman <bagus.solayman@spesolution.com>
2025-09-29 14:28:05 +0700

    feat(ui): Add Component Upload File with drag-and-drop functionality and file preview

---
+++ src/@dront/components/UploadFile/FileUploader.tsx
@@ -0,0 +1,126 @@
+import React, { forwardRef, useState } from 'react';
+import { FormHelperText, FormLabel, TextField } from '@mui/material';
+
+import { handleChange, removePreview } from './actions';
+import initialPreview, { FILE_TYPES } from './constant';
+import DraggableFileUploader from './drag-and-drop';
+import callbackChildren from './helpers/callback-children';
+import Preview from './preview';
+import type { UploadFileProps, FilePreview } from './types';
+
+const FileUploader = forwardRef<HTMLInputElement, UploadFileProps>((props, ref) => {
+  const {
+    children,
+    error,
+    label,
+    id,
+    helperText,
+    onRemove,
+    customPreview,
+    setCustomPreview,
+    draggable = true,
+    showPreview = true,
+    showViewButton = true,
+    readOnly = false,
+    onChange = () => {},
+    breakChange = () => false,
+    fileTypes = 'ALL',
+    ...rest
+  } = props;
+
+  const [preview, setPreview] = useState<FilePreview>(initialPreview);
+
+  const previewState = customPreview || preview;
+  const handlePreview = setCustomPreview || setPreview;
+
+  const { name, size, process, loadingInfo, type, url } = previewState;
+
+  const isShowField = !!process && showPreview;
+
+  const getAcceptTypes = (): string => {
+    return FILE_TYPES[fileTypes] || FILE_TYPES.ALL;
+  };
+
+  return (
+    <>
+      <FormLabel
+        error={error}
+        htmlFor={id}
+        required={rest.required}
+        sx={{ display: 'block', mb: '8px', color: ({ palette }) => palette.common.black }}
+      >
+        {label}
+      </FormLabel>
+
+      {!readOnly && (
+        <DraggableFileUploader
+          acceptTypes={fileTypes}
+          breakChange={breakChange}
+          draggable={draggable}
+          error={error}
+          id={id}
+          isShowField={isShowField}
+          onChange={onChange}
+          setPreview={handlePreview}
+        >
+          {({ getInputProps }) => {
+            if (draggable) {
+              return <input ref={ref} accept={getAcceptTypes()} {...(draggable ? getInputProps() : {})} id={id} />;
+            }
+
+            return (
+              <TextField
+                fullWidth
+                error={error}
+                inputProps={{ accept: getAcceptTypes() }}
+                inputRef={ref}
+                sx={{ display: isShowField ? 'none' : 'block', ...rest.sx }}
+                type="file"
+                onChange={async event => {
+                  if (breakChange(event as React.ChangeEvent<HTMLInputElement>)) return;
+
+                  const files = (event.target as HTMLInputElement).files;
+
+                  if (!files || files.length === 0) return;
+
+                  const file = files[0];
+                  const response = await handleChange(file, handlePreview);
+
+                  onChange(event as React.ChangeEvent<HTMLInputElement>, {
+                    file: response instanceof Error ? undefined : response,
+                    error: response instanceof Error ? response : undefined
+                  });
+                }}
+                {...rest}
+              />
+            );
+          }}
+        </DraggableFileUploader>
+      )}
+
+      {showPreview && (
+        <Preview
+          loadingInfo={loadingInfo}
+          name={name}
+          process={process}
+          removePreview={() => removePreview(handlePreview, onRemove)}
+          showViewButton={showViewButton}
+          readOnly={readOnly}
+          size={size}
+          type={type}
+          url={url}
+        />
+      )}
+
+      <FormHelperText error={error} id={`${id}-helper-text`}>
+        {helperText}
+      </FormHelperText>
+
+      {callbackChildren(children, previewState)}
+    </>
+  );
+});
+
+FileUploader.displayName = 'FileUploader';
+
+export default FileUploader;
---
+++ src/@dront/components/UploadFile/drag-and-drop/index.tsx
@@ -0,0 +1,125 @@
+import React, { useCallback } from 'react';
+import { alpha, Button, Typography } from '@mui/material';
+import { useDropzone } from 'react-dropzone';
+
+import { handleChange } from '../actions';
+import { DragWrapper } from '../file-uploader.styled';
+import callbackChildren from '../helpers/callback-children';
+import type { FilePreview, FileType } from '../types';
+
+interface DraggableFileUploaderProps {
+  id: string;
+  draggable: boolean;
+  children: React.ReactNode | (({ getInputProps }: { getInputProps: () => any }) => React.ReactNode);
+  onChange?: (event: any, errors?: any) => void;
+  setPreview: React.Dispatch<React.SetStateAction<FilePreview>>;
+  isShowField: boolean;
+  error?: boolean;
+  breakChange: (event: any) => boolean;
+  acceptTypes?: FileType;
+}
+
+const DraggableFileUploader: React.FC<DraggableFileUploaderProps> = ({
+  id,
+  draggable,
+  children,
+  onChange,
+  setPreview,
+  isShowField,
+  error,
+  breakChange,
+  acceptTypes = 'ALL'
+}) => {
+  const getAcceptTypes = () => {
+    switch (acceptTypes) {
+      case 'PDF_ONLY':
+        return { 'application/pdf': ['.pdf'] };
+      case 'IMAGE_ONLY':
+        return {
+          'image/jpeg': ['.jpeg', '.jpg'],
+          'image/png': ['.png'],
+          'image/gif': ['.gif'],
+          'image/webp': ['.webp']
+        };
+      case 'EXCEL_ONLY':
+        return {
+          'application/vnd.ms-excel': ['.xls'],
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx']
+        };
+      case 'WORD_ONLY':
+        return {
+          'application/msword': ['.doc'],
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx']
+        };
+      case 'DOCUMENT':
+        return {
+          'application/pdf': ['.pdf'],
+          'application/vnd.ms-excel': ['.xls'],
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
+          'application/msword': ['.doc'],
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx']
+        };
+      default:
+        return undefined;
+    }
+  };
+
+  const onDrop = useCallback(
+    async (files: File[], errors: any) => {
+      if (breakChange({ target: { files } })) return;
+
+      if (files.length > 0) {
+        const compressed = await handleChange(files[0], setPreview);
+
+        onChange?.({ target: { files: [compressed, ...files] } }, errors);
+      } else {
+        onChange?.({ target: { files } }, errors);
+      }
+    },
+    [onChange, setPreview, breakChange]
+  );
+
+  const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
+    onDrop,
+    noClick: true,
+    accept: getAcceptTypes() as any
+  });
+
+  if (!draggable) {
+    return callbackChildren(children, { getInputProps });
+  }
+
+  return (
+    <DragWrapper
+      sx={{
+        display: isShowField ? 'none' : 'flex',
+        backgroundColor: theme => (isDragActive ? alpha(theme.palette.primary.light, 0.2) : theme.palette.grey[100]),
+        backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='${
+          error ? '%23EF2531' : '%23A1BAC4'
+        }' stroke-width='3' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");`
+      }}
+      {...getRootProps()}
+    >
+      {isDragActive ? (
+        <Typography>Drop file here</Typography>
+      ) : (
+        <>
+          <Button
+            size="small"
+            sx={{ width: 'min(140px, 100%)' }}
+            variant="contained"
+            onClick={open}
+            id={`choose-file-${id}`}
+          >
+            Browse
+          </Button>
+          <Typography>or drop file here</Typography>
+        </>
+      )}
+
+      {callbackChildren(children, { getInputProps })}
+    </DragWrapper>
+  );
+};
+
+export default DraggableFileUploader;
